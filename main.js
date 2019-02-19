8406f67f2ea7bab41bab6f3d6c93613223c3211e
Bob Brown <robertbernardbrown@gmail.com>
2019-02-18 19:32:27 -0500

    push changes

--- main.js
+++ main.js
@@ -59,6 +59,7 @@ class Blockchain{
 let jsChain = new Blockchain();
 jsChain.addBlock(new Block("12/25/2017", {amount: 5}));
 jsChain.addBlock(new Block("12/26/2017", {amount: 10}));
+jsChain.addBlock(new Block("12/26/2017", {amount: 10}));
 
 console.log(JSON.stringify(jsChain, null, 4));
 console.log("Is blockchain valid? " + jsChain.checkValid());
